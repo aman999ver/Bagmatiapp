2a6705d3ce272b5d46e6c54fd0365252aca68caa
aman999ver <av4586797@gmail.com>
2025-08-07 16:11:16 +0545

    commit

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -13,6 +14,8 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.MapControllers();
 
 app.UseHttpsRedirection();
 
